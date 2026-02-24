c21ed0e44781c3a834c5b5983e2dcf0feaeb4eb0
Kofi Amo-Antwi <bigboyka@Kofis-MacBook-Pro.local>
2026-02-23 23:34:40 -0600

    tutorial 5

--- tutorial1.ts
+++ tutorial1.ts
@@ -3,22 +3,57 @@
  * 
  */
 
-enum LoginError{
-    Unauthorized = "unauthorized",
-    NoUser = 'wrongcredentials',
-    Internal = "Internal"
+// enum LoginError{
+//     Unauthorized = "unauthorized",
+//     NoUser = 'wrongcredentials',
+//     Internal = "Internal"
+// }
+
+// const printErrorMsg = (error:LoginError) => {
+//     if(error === LoginError.Unauthorized){
+//         console.log("User not authorized");
+//     }
+//     else if(error === LoginError.NoUser){
+//         console.log("Wrong username / password combination");
+//     }
+
+//     else if(error === LoginError.Internal){
+//         console.log("Internal Error");
+//     }
+//     console.log(error);
+// }
+
+
+
+// generics
+
+class StorageContainer<T>{
+   private contents: T[]
+
+   constructor(){
+        this.contents = []
+   }
+
+   addItem(item:T){
+    this.contents.push(item);
+   }
+
+   getItem(idx: number):T | undefined{
+        return this.contents[idx];
+   }
 }
 
-const printErrorMsg = (error:LoginError) => {
-    if(error === LoginError.Unauthorized){
-        console.log("User not authorized");
-    }
-    else if(error === LoginError.NoUser){
-        console.log("Wrong username / password combination");
-    }
+const usernames = new StorageContainer<string>();
 
-    else if(error === LoginError.Internal){
-        console.log("Internal Error");
-    }
-    console.log(error);
-}
+usernames.addItem("Kofi");
+usernames.addItem("benji")
+console.log(usernames.getItem(0));
+
+
+
+
+const friendsCount = new StorageContainer<number>();
+
+friendsCount.addItem(23);
+friendsCount.addItem(678)
+console.log(friendsCount.getItem(0));
