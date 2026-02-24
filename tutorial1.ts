250662d9df08c6eeb8013cc0e65eba72b2d16bec
Kofi Amo-Antwi <bigboyka@Kofis-MacBook-Pro.local>
2026-02-23 21:28:01 -0600

    tutorial 1 code

---
+++ tutorial1.ts
@@ -0,0 +1,21 @@
+// type anotations in typescript
+
+// let id:number = 5; //typescript adds defintion instead of javascript assuming types.
+// let company :string = "Notion"; // string
+
+// let isIPO: boolean = true; // boolean 
+
+
+// let ids:number[] = [1,2,3,4];
+// let x: any="period"; // could be any type
+
+// let yArray: any[] = ["kofi",0,true];  // any array type
+
+
+const concatenateValues = (a:string,b:string):string=>{
+    return a + b;
+}
+
+console.log(concatenateValues("Hello","World"));
+console.log(concatenateValues("10","10"));
+
